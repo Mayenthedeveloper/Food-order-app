24f9a594d7cfb9fd3bb4dbad72a6ce4cce2ccb5f
Mayen Akpan <neroxma@yahoo.com>
2021-12-04 22:28:36 -0800

    Added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import Navbar from "./components/Navbar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      Hello World <h1>another one</h1>
+      <Router>
+        <Navbar />
+      </Router>
     </div>
   );
 }
